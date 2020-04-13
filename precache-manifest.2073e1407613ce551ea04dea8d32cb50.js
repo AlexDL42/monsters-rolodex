d71dbd0a10c5ac5c925292243d1d8b9af4079dcc
AlexDL42 <adl42.pro@gmail.com>
2020-04-13 16:55:07 +0100

    Updates

---
+++ precache-manifest.2073e1407613ce551ea04dea8d32cb50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66df83146d0d328415dce0d8c582074a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "294c7e1baca2819810a0",
+    "url": "/monsters-rolodex/static/css/main.35c6af94.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "294c7e1baca2819810a0",
+    "url": "/monsters-rolodex/static/js/main.4bcddec2.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
